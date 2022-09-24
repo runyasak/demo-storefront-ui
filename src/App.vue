fe02f1458836672f82c22c33024751dba79e5918
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-09-24 15:48:37 +0700

    feat: implement SfImage on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
+  <SfImage
+    src="/assets/storybook/SfImage/product-216x326.jpg"
+    :srcsets="[
+      {
+        src: '/assets/storybook/SfImage/product-109x164.webp',
+        width: 109,
+        breakpoint: 480,
+      },
+      {
+        src: '/assets/storybook/SfImage/product-216x326.jpg',
+        width: 1200,
+        breakpoint: 1200,
+      },
+      {
+        src: '/assets/storybook/SfImage/product-109x164.webp',
+        width: '400px',
+        breakpoint: 768,
+      },
+    ]"
+    alt="Vila stripe maxi shirt dress"
+    :width="null"
+    :height="null"
+  />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
+import { SfImage } from '@storefront-ui/vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    SfImage,
   },
 };
 </script>
